69f8ee0616e93d1436fee2c996b5b07d07924ff7
Baptiste Martin <81614331+baptiste-martin-campus@users.noreply.github.com>
2021-09-07 16:58:17 +0200

    init tri

--- js/triVille.js
+++ js/triVille.js
@@ -3,27 +3,27 @@ let listVille = [];
 let nbPermutation = 0;
 let nbComparaison = 0;
 
-document.querySelector("#read-button").addEventListener('click', function () {
-    csvFile = document.querySelector("#file-input").files[0];
-    let reader = new FileReader();
-    reader.addEventListener('load', function (e) {
-        // récupération de la liste des villes
-        listVille = getArrayCsv(e.target.result);
+document.querySelector("#read-button").addEventListener('click', function() {
+  csvFile = document.querySelector("#file-input").files[0];
+  let reader = new FileReader();
+  reader.addEventListener('load', function(e) {
+    // récupération de la liste des villes
+    listVille = getArrayCsv(e.target.result);
 
-        // Calcul de la distance des villes par rapport à Grenoble
-        listVille.forEach(ville => {
-            ville.distanceFromGrenoble = distanceFromGrenoble(ville);
-        });
-        // Tri
-        const algo = $("#algo-select").val();
-        nbPermutation = 0;
-        nbComparaison = 0;
-        sort(algo);
-
-        // Affichage 
-        displayListVille()
+    // Calcul de la distance des villes par rapport à Grenoble
+    listVille.forEach(ville => {
+      ville.distanceFromGrenoble = distanceFromGrenoble(ville);
     });
-    reader.readAsText(csvFile)
+    // Tri
+    const algo = $("#algo-select").val();
+    nbPermutation = 0;
+    nbComparaison = 0;
+    sort(algo);
+
+    // Affichage
+    displayListVille()
+  });
+  reader.readAsText(csvFile)
 })
 
 /**
@@ -32,27 +32,27 @@ document.querySelector("#read-button").addEventListener('click', function () {
  * @returns la liste des villes mis en forme
  */
 function getArrayCsv(csv) {
-    let listLine = csv.split("\n")
-    listVille = [];
-    let isFirstLine = true;
-    listLine.forEach(line => {
-        if (isFirstLine || line === '') {
-            isFirstLine = false;
-        } else {
-            let listColumn = line.split(";");
-            listVille.push(
-                new Ville(
-                    listColumn[8],
-                    listColumn[9],
-                    listColumn[11],
-                    listColumn[12],
-                    listColumn[13],
-                    0
-                )
-            );
-        }
-    });
-    return listVille;
+  let listLine = csv.split("\n")
+  listVille = [];
+  let isFirstLine = true;
+  listLine.forEach(line => {
+    if (isFirstLine || line === '') {
+      isFirstLine = false;
+    } else {
+      let listColumn = line.split(";");
+      listVille.push(
+        new Ville(
+          listColumn[8],
+          listColumn[9],
+          listColumn[11],
+          listColumn[12],
+          listColumn[13],
+          0
+        )
+      );
+    }
+  });
+  return listVille;
 }
 
 /**
@@ -60,9 +60,26 @@ function getArrayCsv(csv) {
  * @param ville ville
  * @returns la distance qui sépare la ville de Grenoble
  */
+
+
 function distanceFromGrenoble(ville) {
-    console.log('implement me !');
-    return 0;
+  let lat2 = ville.latitude;
+  let lon2 = ville.longitude;
+  const lat1 = 45.188529;
+  const lon1 = 5.724524;
+  const R = 6371e3; // metres
+  const φ1 = lat1 * Math.PI/180; // φ, λ in radians
+  const φ2 = lat2 * Math.PI/180;
+  const Δφ = (lat2-lat1) * Math.PI/180;
+  const Δλ = (lon2-lon1) * Math.PI/180;
+
+  const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ/2) * Math.sin(Δλ/2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+  const d = R * c; // in metres
+  return d;
 }
 
 /**
@@ -73,100 +90,113 @@ function distanceFromGrenoble(ville) {
  * @return vrai si la ville i est plus proche
  */
 function isLess(i, j) {
-    console.log('implement me !');
+  if (listVille[i] < listVille[j]) {
     return true;
+  }
 }
 
 /**
  * interverti la ville i avec la ville j dans la liste des villes
- * @param {*} i 
- * @param {*} j 
+ * @param {*} i
+ * @param {*} j
  */
 function swap(i, j) {
-    console.log('implement me !');
+  let temp;
+  temp = i;
+  i = j;
+  j = temp;
 }
 
 function sort(type) {
-    switch (type) {
-        case 'insert':
-            insertsort();
-            break;
-        case 'select':
-            selectionsort();
-            break;
-        case 'bubble':
-            bubblesort();
-            break;
-        case 'shell':
-            shellsort();
-            break;
-        case 'merge':
-            mergesort();
-            break;
-        case 'heap':
-            heapsort();
-            break;
-        case 'quick':
-            quicksort();
-            break;
-    }
+  switch (type) {
+    case 'insert':
+      insertsort(listVille);
+      break;
+    case 'select':
+      selectionsort();
+      break;
+    case 'bubble':
+      bubblesort();
+      break;
+    case 'shell':
+      shellsort();
+      break;
+    case 'merge':
+      mergesort();
+      break;
+    case 'heap':
+      heapsort();
+      break;
+    case 'quick':
+      quicksort();
+      break;
+  }
 }
 
-function insertsort() {
-    console.log("insertsort - implement me !");
-}
+function insertsort(listVille) {
+    let n = listVille.length
+    for (let i = 1; i < n; i++) {
+      let temp = listVille[i];
+      let j = i;
+      while (j > 0 && listVille[j - 1] > temp) {
+        listVille[j] = listVille[j - 1]
+        j = j - 1;
+      }
+      listVille[j] = temp;
+    }
+  }
 
 function selectionsort() {
-    console.log("selectionsort - implement me !");
+  console.log("selectionsort - implement me !");
 }
 
 function bubblesort() {
-    console.log("bubblesort - implement me !");
+  console.log("bubblesort - implement me !");
 }
 
 function shellsort() {
-    console.log("shellsort - implement me !");
+  console.log("shellsort - implement me !");
 }
 
 function mergesort() {
-    console.log("mergesort - implement me !");
+  console.log("mergesort - implement me !");
 }
 
 
 function heapsort() {
-    console.log("heapsort - implement me !");
+  console.log("heapsort - implement me !");
 }
 
 function quicksort() {
-    console.log("quicksort - implement me !");
+  console.log("quicksort - implement me !");
 }
 
 /** MODEL */
 
 class Ville {
-    constructor(nom_commune, codes_postaux, latitude, longitude, dist, distanceFromGrenoble) {
-        this.nom_commune = nom_commune;
-        this.codes_postaux = codes_postaux;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.dist = dist;
-        this.distanceFromGrenoble = distanceFromGrenoble;
-    }
+  constructor(nom_commune, codes_postaux, latitude, longitude, dist, distanceFromGrenoble) {
+    this.nom_commune = nom_commune;
+    this.codes_postaux = codes_postaux;
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.dist = dist;
+    this.distanceFromGrenoble = distanceFromGrenoble;
+  }
 }
 
 /** AFFICHAGE */
 function displayPermutation(nbPermutation) {
-    document.getElementById('permutation').innerHTML = nbPermutation + ' permutations';
+  document.getElementById('permutation').innerHTML = nbPermutation + ' permutations';
 }
 
 function displayListVille() {
-    document.getElementById("navp").innerHTML = "";
-    displayPermutation(nbPermutation);
-    let mainList = document.getElementById("navp");
-    for (var i = 0; i < listVille.length; i++) {
-        let item = listVille[i];
-        let elem = document.createElement("li");
-        elem.innerHTML = item.nom_commune + " - \t" + Math.round(item.distanceFromGrenoble * 100) / 100 + ' m';
-        mainList.appendChild(elem);
-    }
+  document.getElementById("navp").innerHTML = "";
+  displayPermutation(nbPermutation);
+  let mainList = document.getElementById("navp");
+  for (var i = 0; i < listVille.length; i++) {
+    let item = listVille[i];
+    let elem = document.createElement("li");
+    elem.innerHTML = item.nom_commune + " - \t" + Math.round(item.distanceFromGrenoble * 100) / 100 + ' m';
+    mainList.appendChild(elem);
+  }
 }
